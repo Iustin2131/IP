166e5f2042200b5c92c47fe829e161f9ff8e18f6
Iustin2131 <126079873+Iustin2131@users.noreply.github.com>
2024-04-23 02:16:06 +0300

    ip

---
+++ react-typescript-cra-navbar-component-master/src/components/product/Product.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './Product.css';
+
+type ProductProps = {
+  name: string;
+  imageSrc: string;
+  initialQuantity: number;
+};
+
+const Product: React.FC<ProductProps> = ({ name, imageSrc, initialQuantity }) => {
+  const [quantity, setQuantity] = useState(initialQuantity);
+
+  const decreaseQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const buyProduct = () => {
+    if (quantity > 0) {
+      alert(`You bought ${quantity} item(s) of ${name}!`);
+      setQuantity(0);
+    } else {
+      alert('Please select quantity before buying.');
+    }
+  };
+
+  return (
+    <div className="product">
+      <img src={imageSrc} alt={name} className="product-image" />
+      <div className="product-info">
+        <h2 className="product-name">{name}</h2>
+        <div className="quantity-controls">
+          <button className="quantity-button" onClick={decreaseQuantity}>-</button>
+          <span className="current-quantity">{quantity}</span>
+          <button className="quantity-button" onClick={increaseQuantity}>+</button>
+        </div>
+        <button className="buy-button" onClick={buyProduct}>Buy</button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
